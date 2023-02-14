cf47d7aa24dbbbc4b2a496a9c8a880d415362ffd
Ndfj79 <107001737+Ndfj79@users.noreply.github.com>
2023-02-14 09:21:46 +0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,258 @@
+let albums = [
+    {
+        title:'Хорошая музыка',
+        img:'assets/first.jpg',
+        description: 'Просто музыка для занятий',
+        year: 'Выпущен 10.02.2023',
+        tracks: [
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+        ]
+    },
+    {
+        title:'Лучшая музыка',
+        img:'assets/second.jpg',
+        description: 'Послушай и сам всё поймешь',
+        year: 'Выпущен 10.02.2023',
+        tracks: [
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+        ]
+    },
+    {
+        title:'Прекрасная музыка',
+        img:'assets/third.jpg',
+        description: 'Эта музыка позволяет почувствовать умиротворение',
+        year: 'Выпущен 10.02.2023',
+        tracks: [
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+        ]
+    },
+    {
+        title:'Отличная музыка',
+        img:'assets/fourth.jpg',
+        description: 'Музыка для поездки или для бега',
+        year: 'Выпущен 10.02.2023',
+        tracks: [
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+            {
+                title: `Топовая песя`,
+                author: `Моцарт`,
+                time: `1:01`,
+                durationSeconds: 61, 
+                src: 'music/bad_pigges.mp3',
+                isPlaying: false
+            },
+        ]
+    }
+];
